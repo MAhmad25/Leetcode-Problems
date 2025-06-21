e1ba7e9953eacc720b0edbedba988a82841cc40f
MUHAMMAD AHMAD <userarenotproduct@gmail.com>
2025-06-21 11:39:54 +0500

    Time: 0 ms (100.00%) | Memory: 60.4 MB (56.30%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.js
@@ -0,0 +1,9 @@
+const moveZeroes = function(nums) {
+  let lastNonZero = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      [nums[lastNonZero], nums[i]] = [nums[i], nums[lastNonZero]];
+      lastNonZero++;
+    }
+  }
+};
