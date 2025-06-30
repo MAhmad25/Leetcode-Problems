5a0705fbeeb5e945061f86f9e78110f725fc84fe
MUHAMMAD AHMAD <userarenotproduct@gmail.com>
2025-06-30 21:11:23 +0500

    Time: 12 ms (72.77%) | Memory: 55 MB (96.67%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -1,4 +1,18 @@
 const isAnagram = function (s, t) {
+    /*Solution1
     if (s.length !== t.length) return false;
     return s.split("").sort().join("") === t.split("").sort().join("");
+    */
+    // Solution2
+    if (s.length !== t.length) return false;
+    const sObject = {};
+    const tObject = {};
+    for (let i = 0; i < s.length; i++) {
+        sObject[s[i]] = (sObject[s[i]] || 0) + 1;
+        tObject[t[i]] = (tObject[t[i]] || 0) + 1;
+    }
+    for (const key in sObject) {
+        if (sObject[key] !== tObject[key]) return false;
+    }
+    return true;
 };
