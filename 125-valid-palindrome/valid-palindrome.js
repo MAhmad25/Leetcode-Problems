7245e16db0e6a8b98e322b7384e7570d8f87a18e
MUHAMMAD AHMAD <userarenotproduct@gmail.com>
2025-07-13 14:04:07 +0500

    Time: 3 ms (86.17%) | Memory: 57.8 MB (58.11%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -1,11 +1,4 @@
 const isPalindrome = function (s) {
-    const res = s.replace(/[^0-9a-zA-Z]/g, "").toLowerCase();
-    let l = 0,
-        h = res.length - 1;
-    while (l < h) {
-        if (res[l] !== res[h]) return false;
-        h--;
-        l++;
-    }
-    return true;
+    const regex = s.replace(/[^0-9a-zA-Z]/g, "");
+    return regex.toLowerCase() === regex.split("").reverse().join("").toLowerCase()
 };
