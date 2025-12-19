f6190271f1380f7cb053d947f57af566aff2c486
MUHAMMAD AHMAD <userarenotproduct@gmail.com>
2025-12-20 01:10:42 +0500

    Time: 0 ms (100.00%) | Memory: 56 MB (91.72%) - LeetSync

--- 136-single-number/single-number.ts
+++ 136-single-number/single-number.ts
@@ -1,5 +1,7 @@
 function singleNumber(nums: number[]): number {
-    const count:Object = {};
-    for (const char of nums) count[char] = (count[char] || 0) + 1;
-    for (const [number, freq] of Object.entries(count)) if (freq === 1) return Number(number);
+    let xor: number = 0;
+    for (let i: number = 0; i < nums.length; i++) {
+        xor = xor ^ nums[i];
+    }
+    return xor;
 };
