36b015b78a493015c4ef58edc9f8a018f42566cc
MUHAMMAD AHMAD <itzahmad.dev@gmail.com>
2026-01-17 00:04:17 +0500

    Time: 0 ms (100.00%) | Memory: 56.6 MB (67.86%) - LeetSync

---
+++ 1-two-sum/two-sum.ts
@@ -0,0 +1,10 @@
+function twoSum(nums: number[], target: number): number[] {
+    const obj = {};
+    for (let i = 0; i < nums.length; i++) {
+        let n = nums[i];
+        if (obj[target - n]>=0) {
+            return [obj[target - n], i];
+        } else obj[n] = i;
+    }
+
+};
