af9c46c26564a64a39c8b9a754f7dbb5bb52e367
MUHAMMAD AHMAD <itzahmad.dev@gmail.com>
2026-03-28 17:30:35 +0500

    Time: 170 ms (16.69%) | Memory: 19.7 MB (36.91%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,30 +1,22 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-      def find_max(freq):
-        return max(freq.values()) if freq else 0
-
-      freq = {}
-      low = 0
-      res = 0
-
-      for high in range(len(s)):
-            ch = s[high]
-            freq[ch] = freq.get(ch, 0) + 1
-
-            ch_most_repeated = find_max(freq)
-            length = high - low + 1
-            character_to_replace = length - ch_most_repeated
-
-            while character_to_replace > k:
-                left_ch = s[low]
-                freq[left_ch] -= 1
-                if freq[left_ch] == 0:
-                    del freq[left_ch]
-                low += 1
-
-                ch_most_repeated = find_max(freq)
-                length = high - low + 1
-                character_to_replace = length - ch_most_repeated
-
-            res = max(res, high - low + 1)
-      return res
+        def find_max_freq(freq):
+            return max(freq.values()) if freq else 0
+        low=0
+        freq={}
+        result=0
+        for high in range(len(s)):
+            freq[s[high]]=freq.get(s[high],0)+1
+            big_value_in_freq=find_max_freq(freq)
+            win_len=high-low+1
+            no_of_ch_replaceable=win_len-big_value_in_freq    
+            while no_of_ch_replaceable>k:
+                freq[s[low]]-=1
+                if freq[s[low]]==0:
+                    del freq[s[low]]
+                low+=1
+                big_value_in_freq=find_max_freq(freq)
+                win_len=high-low+1
+                no_of_ch_replaceable=win_len-big_value_in_freq
+            result=max(result,high-low+1)
+        return result                        
